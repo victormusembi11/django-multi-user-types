aeea88dcb6463888b9f61b64ef3e6aa645e6034e
victormusembi11 <victor.musembi11@gmail.com>
2022-12-21 02:19:51 +0300

    feat: check user logged in is teacher

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -15,3 +15,17 @@ def student_required(
     if function:
         return actual_decorator(function)
     return actual_decorator
+
+
+def teacher_required(
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url="login"
+):
+    """Check that logged in user is a teacher. Redirect to login page if necessary."""
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_teacher,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name,
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
